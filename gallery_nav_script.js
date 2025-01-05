4c6d91a4a83301ad48b0bd1e59706e5a6d8be59f
nativ18 <nativ18@gmail.com>
2025-01-05 20:39:41 +0200

    Update gallery_nav_script.js

--- gallery_nav_script.js
+++ gallery_nav_script.js
@@ -3,14 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
     // Arrays of image URLs for the left and right sides
     const leftImages = [
         "https://images.squarespace-cdn.com/content/6769debcb704bd19bef657cf/8fe80bc2-891c-416a-8cd1-5a0e69e6d8b2/before.jpeg?content-type=image%2Fjpeg",
-        "https://images.squarespace-cdn.com/content/6769debcb704bd19bef657cf/ab28893c-9f78-499b-b56d-05111e2544f3/before_gabot.jpeg?content-type=image%2Fjpeg",
-        "https://images.squarespace-cdn.com/content/6769debcb704bd19bef657cf/another-left-image.jpeg"
+        "https://images.squarespace-cdn.com/content/6769debcb704bd19bef657cf/ab28893c-9f78-499b-b56d-05111e2544f3/before_gabot.jpeg?content-type=image%2Fjpeg"
     ];
 
     const rightImages = [
         "https://images.squarespace-cdn.com/content/6769debcb704bd19bef657cf/50a99eeb-f371-400f-91a1-9a7799d9b540/after.jpeg?content-type=image%2Fjpeg",
-        "https://images.squarespace-cdn.com/content/6769debcb704bd19bef657cf/63bc5f2b-6387-4eb8-b9eb-6aec6ebb6c74/after_gabot.jpeg?content-type=image%2Fjpeg",
-        "https://images.squarespace-cdn.com/content/6769debcb704bd19bef657cf/another-right-image.jpeg"
+        "https://images.squarespace-cdn.com/content/6769debcb704bd19bef657cf/63bc5f2b-6387-4eb8-b9eb-6aec6ebb6c74/after_gabot.jpeg?content-type=image%2Fjpeg"
     ];
 
     let currentIndex = 0; // Start with the first set of images
