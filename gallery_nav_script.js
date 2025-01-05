0070b6b5f6b1df95a97a1e71e56546aa2dafb61f
nativ18 <nativ18@gmail.com>
2025-01-05 21:03:23 +0200

    Update gallery_nav_script.js

--- gallery_nav_script.js
+++ gallery_nav_script.js
@@ -11,6 +11,18 @@ document.addEventListener("DOMContentLoaded", function() {
         "https://images.squarespace-cdn.com/content/6769debcb704bd19bef657cf/63bc5f2b-6387-4eb8-b9eb-6aec6ebb6c74/after_gabot.jpeg?content-type=image%2Fjpeg"
     ];
 
+    const titleText = [
+        "טשטוש קמטים בגבות",
+        "טשטוש קמטי עיניים",
+    ];
+
+    const descriptionText = [
+        "טיפול טשטוש קמטים כולל הזרקת חומרים אסתטיים כמו חומצה היאלורונית או בוטוקס להחלקת קמטים ושיפור מראה עור הפנים. הטיפול מתאים למגוון קמטים ונותן תוצאה טבעית ורעננה. התוצאות נשארות למשך מספר חודשים וניתן לחדש את הטיפול לפי הצורך.
+",
+        "טיפול טשטוש קמטים בעיניים מיועד להפחתת הקמטים הדקים והקווים שמופיעים סביב העיניים, כגון "רגלי עורב". בעזרת הזרקות חומרים כמו חומצה היאלורונית או בוטוקס, אנו מספקים פתרון עדין וממוקד להחלקת הקמטים ולהחזרת המראה הצעיר והרענן לעור סביב העיניים. התוצאה היא מראה חלק וצעיר יותר, עם שיפור במרקם עור העפעפיים והסביבה.
+"
+    ];
+
     let currentIndex = 0; // Start with the first set of images
 
     // Get references to the img-left and img-right elements
@@ -26,6 +38,17 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("Right image src:", imgRight.src);
     }
 
+    function updateTitle(index) {
+        console.log("Updating title to index:", index);
+        treatmentTitleText.src = titleText[index];
+    }
+
+    
+    function updateDescription(index) {
+        console.log("Updating description to index:", index);
+        descriptionTitleText.src = descriptionText[index];
+    }
+
     // Add event listeners to buttons (ensure they are present in the HTML)
     document.getElementById("prev-btn").addEventListener("click", function() {
         console.log("Previous button clicked");
@@ -37,5 +60,15 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("Next button clicked");
         currentIndex = (currentIndex + 1) % leftImages.length; // Wrap around to the first image if needed
         updateImages(currentIndex);
+    });
+
+    document.getElementById("treatment-title-text").addEventListener("click", function() {
+        currentIndex = (currentIndex + 1) % leftImages.length; // Wrap around to the first image if needed
+        updateTitle(currentIndex);
+    });
+
+    document.getElementById("treatment-description-text").addEventListener("click", function() {
+        currentIndex = (currentIndex + 1) % leftImages.length; // Wrap around to the first image if needed
+        updateDescription(currentIndex);
     });
 });
