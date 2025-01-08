8bef2cf8149b54fc5f546d48c94b557c253faeca
nativ18 <nativ18@gmail.com>
2025-01-08 22:31:50 +0200

    Update image-comparison-slider.js

--- image-comparison-slider.js
+++ image-comparison-slider.js
@@ -10,8 +10,8 @@
         var height = imgLeft.getBoundingClientRect().height;
 
         // Initialize mover position and clipping
-        mover.style.left = width / 2 - moverWidth / 2 + "px";
-        imgLeft.style.clip = "rect(0px, " + width / 2 + "px, 999px, 0px)";
+        mover.style.left = width / 2 - moverWidth / 2 - 10 + "px";
+        imgLeft.style.clip = "rect(0px, " + width / 2 - 5 + "px, 999px, 0px)";
 
         // Add cursor styling explicitly
         mover.style.cursor = "grab";
@@ -82,8 +82,8 @@
             var height = imgLeft.getBoundingClientRect().height;
 
             // Reset position and clipping
-            mover.style.left = width / 2 - moverWidth / 2 + "px";
-            imgLeft.style.clip = "rect(0px, " + width / 2 + "px, " + height + "px, 0px)";
+            mover.style.left = width / 2 - moverWidth / 2 - 10 + "px";
+            imgLeft.style.clip = "rect(0px, " + width / 2 - 5 + "px, " + height + "px, 0px)";
         }
     });
 })();
